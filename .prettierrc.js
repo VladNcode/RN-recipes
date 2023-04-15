33b8118ed5f3a37ef305d52ecb67abe032b9bcad
vladncode <vladncode@gmail.com>
2023-04-15 19:06:20 +0300

    Added linters and assets

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  printWidth: 120,
+  endOfLine: 'auto',
+  bracketSameLine: true,
+};
